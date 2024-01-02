daa1103dafad3b1765d96e932e13b2b43264e0ce
ziad hatem <ziadhatem2022@gmail.com>
2024-01-02 18:57:51 +0200

    first commit

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,14 @@
-import NextAuth from 'next-auth';
-import Github from 'next-auth/providers/github';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { db } from '@/db';
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/db";
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
-  throw new Error('Missing github oauth credentials');
+  throw new Error("Missing github oauth credentials");
 }
 
 export const {
@@ -18,7 +19,7 @@ export const {
 } = NextAuth({
   adapter: PrismaAdapter(db),
   providers: [
-    Github({
+    GithubProvider({
       clientId: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
     }),
